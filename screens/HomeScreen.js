00e1909abcacbbe1e8357a0f8c696ef22ce26b75
john-kloss <jonathan.kloss1@gmail.com>
2018-10-17 15:27:26 +0200

    moved items to settings screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,7 @@
 import React from "react";
-import {
-  View,
-  Text,
-  Button,
-  Modal,
-  TouchableHighlight,
-  StyleSheet
-} from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 
 export default class HomeScreen extends React.Component {
-  state = {
-    items: [
-      { title: "Birkenpollen", selected: true },
-      { title: "CO2", selected: false },
-      { title: "Wurst", selected: false }
-    ],
-    modalVisible: false
-  };
   static navigationOptions = {
     title: "Home",
     headerStyle: {
@@ -28,43 +13,14 @@ export default class HomeScreen extends React.Component {
     }
   };
 
-  // TODO: update state
-  onItemPressed(item) {
-    let items = this.state.items;
-
-    this.setState({
-      items
-    });
-  }
-
   configure = () => {};
   render() {
     return (
       <View style={styles.container}>
         <Button
-          title="Airchecker anpassen"
-          onPress={() => this.setState({ modalVisible: true })}
-          // onPress={() => this.props.navigation.navigate("SettingsScreen", {})}
+          title="Einstellungen"
+          onPress={() => this.props.navigation.navigate("SettingsScreen")}
         />
-
-        <Modal
-          visible={this.state.modalVisible}
-          onRequestClose={() => this.setState({ modalVisible: false })}
-        >
-          <View style={styles.rootView}>
-            {this.state.items.map(item => (
-              <TouchableHighlight
-                onPress={() => this.onItemPressed(item)}
-                underlayColor="#f00"
-                key={item.title}
-              >
-                <Text style={styles.text}>
-                  {item.title + " " + item.selected}
-                </Text>
-              </TouchableHighlight>
-            ))}
-          </View>
-        </Modal>
       </View>
     );
   }
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,60 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, TouchableHighlight, Text } from "react-native";
 export default class SettingsScreen extends React.Component {
-  state = {};
+  static navigationOptions = {
+    title: "Einstellungen",
+    headerStyle: {
+      backgroundColor: "#f4511e"
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: "bold"
+    }
+  };
+
+  state = {
+    items: [
+      { title: "Birkenpollen", selected: true },
+      { title: "CO2", selected: false },
+      { title: "Wurst", selected: false }
+    ]
+  };
+
+  onItemPressed(item) {
+    let items = this.state.items;
+    let newItem = items.find(i => {
+      return i.title === item.title;
+    });
+    newItem.selected = !newItem.selected;
+    this.setState({
+      items
+    });
+  }
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        {this.state.items.map(item => (
+          <TouchableHighlight
+            onPress={() => this.onItemPressed(item)}
+            underlayColor="#f00"
+            key={item.title}
+          >
+            <Text style={styles.text}>{item.title + " " + item.selected}</Text>
+          </TouchableHighlight>
+        ))}
+      </View>
+    );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    padding: 16,
+    fontSize: 30
+  }
+});
