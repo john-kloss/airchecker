1c36e738bc57ce3ff0e3f4f16bd5a3983afa5738
john-kloss <jonathan.kloss1@gmail.com>
2018-12-05 15:31:26 +0100

    Top header

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -9,29 +9,44 @@ import {
   ScrollView
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import Toast from 'react-native-simple-toast';
-
+import Toast from "react-native-simple-toast";
 
 export default class SettingsScreen extends React.Component {
-  static navigationOptions = {
-    
-    title: "Einstellungen",
-    headerStyle: {
-      backgroundColor: "#f4511e"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle: {
-      fontWeight: "bold"
-    }
+  state = {};
+  static navigationOptions = ({ navigation }) => {
+    const { state } = navigation;
+    const params = state.params || {};
+    return {
+      title: "Einstellungen",
+      headerStyle: {
+        backgroundColor: "#f4511e"
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold"
+      },
+      headerLeftContainerStyle: { paddingLeft: 10 },
+      headerLeft: (
+        <Ionicons
+          name="md-arrow-back"
+          size={30}
+          onPress={params.goBack}
+          color="white"
+        />
+      )
+    };
   };
 
   componentWillMount() {
     this.setState({ items: this.props.navigation.state.params.items });
+    this.props.navigation.setParams({
+      goBack: this.goBack
+    });
   }
 
   // change if item is selected or not
   onItemPressed(item) {
-    Toast.show('item: ' + item.title+ "invisible");
+    Toast.show("item: " + item.title + "invisible");
     let items = this.state.items;
     let newItem = items.find(i => {
       return i.title === item.title;
@@ -43,9 +58,10 @@ export default class SettingsScreen extends React.Component {
   }
 
   onSliderChanged(item, parameter, value) {
-    Toast.show('item: ' + item.title + ' parameter: ' + parameter + ' value: ' + value);
+    Toast.show(
+      "item: " + item.title + " parameter: " + parameter + " value: " + value
+    );
 
-    
     let items = this.state.items;
     let newItem = items.find(i => {
       return i.title === item.title;
@@ -57,9 +73,8 @@ export default class SettingsScreen extends React.Component {
   }
 
   resetTreshold(item) {
-    Toast.show(item.title + ' Reset ');
+    Toast.show(item.title + " Reset ");
 
-    
     let items = this.state.items;
     let newItem = items.find(i => {
       return i.title === item.title;
@@ -73,17 +88,12 @@ export default class SettingsScreen extends React.Component {
 
   goBack = () => {
     const { navigation } = this.props;
-    navigation.goBack();
     navigation.state.params.onUpdate(this.state.items);
+    navigation.goBack();
   };
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Button
-          title="Übernehmen"
-          onPress={() => this.props.navigation.goBack()}
-        />
-
         {this.state.items.map(item => (
           <View style={{ flex: 1 }} key={item.title}>
             <View style={{ flexDirection: "row" }}>
@@ -101,25 +111,45 @@ export default class SettingsScreen extends React.Component {
                 <Text style={styles.text}>{item.title}</Text>
               </View>
             </View>
-            {item.visible && <Text style={styles.text}>{"threshold1: " + item.threshold1}</Text>}
-            {item.visible && <Slider minimumValue={0}
-                                     maximumValue={item.threshold2} 
-                                     step={5}
-                                     value={item.threshold1} 
-                                     onSlidingComplete={(value) => this.onSliderChanged(item, "threshold1", value)}
-                                     />}
+            {item.visible && (
+              <Text style={styles.text}>
+                {"threshold1: " + item.threshold1}
+              </Text>
+            )}
+            {item.visible && (
+              <Slider
+                minimumValue={0}
+                maximumValue={item.threshold2}
+                step={5}
+                value={item.threshold1}
+                onSlidingComplete={value =>
+                  this.onSliderChanged(item, "threshold1", value)
+                }
+              />
+            )}
 
-            {item.visible && <Text style={styles.text}>{"threshold2: " + item.threshold2}</Text>}
-            {item.visible && <Slider minimumValue={item.threshold1} 
-                                     maximumValue={1000} 
-                                     step={5}
-                                     value={item.threshold2} 
-                                     onSlidingComplete={(value) => this.onSliderChanged(item, "threshold2", value)}
-                                     />}
-            {item.visible && <Button
-              title="Zurücksetzen"
-              onPress={() => this.resetTreshold(item)}
-            />}
+            {item.visible && (
+              <Text style={styles.text}>
+                {"threshold2: " + item.threshold2}
+              </Text>
+            )}
+            {item.visible && (
+              <Slider
+                minimumValue={item.threshold1}
+                maximumValue={1000}
+                step={5}
+                value={item.threshold2}
+                onSlidingComplete={value =>
+                  this.onSliderChanged(item, "threshold2", value)
+                }
+              />
+            )}
+            {item.visible && (
+              <Button
+                title="Zurücksetzen"
+                onPress={() => this.resetTreshold(item)}
+              />
+            )}
           </View>
         ))}
       </ScrollView>
